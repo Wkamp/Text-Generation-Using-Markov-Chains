70d1681ea5f072732cabb6815825282cf782ed1a
Wkamp <abscruffy@gmail.com>
2023-06-23 22:06:27 -0400

    Words can now be correctly read into the graph (proper weighting has still not been added)

--- main.cpp
+++ main.cpp
@@ -9,35 +9,54 @@
 using namespace std;
 
 int main() {
-    Graph correctOutput;
-
-    correctOutput.addEdge("Terrible", "sucks", 2);
-    correctOutput.addEdge("You", "hi", 3);
-    correctOutput.addEdge("You", "hi", 3);
-    correctOutput.addEdge("Hello", "C", 1);
-
     Graph graph;
-    fstream testText;
-    testText.open("text/test.txt", ios::in);
+    fstream text;
+    vector<string> words;
+    text.open("text/test.txt", ios::in);
     
-    string temp;
-    
-    if (testText.is_open()) {
-        string word;
-        while (getline(testText, word, ' ')) {
-            if (temp.empty()) {
-                temp = word;
+    // reads text file and pushes each word into words vector
+    if (text.is_open()) {
+        string line;
+        string word = "";
+
+        // setting ' ' as the third argument to getline can separate out each word from the line
+        // however, it was causing issues when adding each word to the graph so I did it manually 
+        while (getline(text, line)) {
+            if (line.size() == 0) { // adds " " to words so I can account for empty lines separating paragraphs
+                words.push_back(" ");
+                continue;
             }
 
-            else {
-                graph.addEdge(temp, word, 1);
-                temp = word;
+            line += ' '; // added so the last word is still pushed to words
+
+
+            for (int i = 0; i < line.size(); i++) {
+                if (line[i] == ' ' && word != "") {
+                    words.push_back(word);
+                    word = "";
+                }
+                else {
+                    word += line[i];
+                    if (word[0] == ' ') {
+                        word = "";
+                    }
+                }
             }
         }
     }
-    testText.close();
-    
-    correctOutput.print();
+    text.close();
+
+    // cout << "Words List: " << "\n";
+    // for (int i = 0; i < words.size(); i++) {
+    //     cout << words[i] << "\n";
+    // }
+
+	for (int i = 0; i < words.size()-1; i++) {
+        if (words[i] != " " && words[i+1] != " ") { // keeps the last word of a paragraph from being adjacent to the beginning word of the next paragraph
+            graph.addEdge(words[i], words[i+1], 1);
+        }
+	}
+
     graph.print();
 
 
